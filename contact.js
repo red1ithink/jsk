531db0d9f590c31fafc372b258c3c5f64f092541
red1ithink <wntjd833@khu.ac.kr>
2024-11-18 05:48:08 +0900

    contact page

---
+++ contact.js
@@ -0,0 +1,6 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for reaching out! I will get back to you soon.");
+    this.reset();
+  });
+  
