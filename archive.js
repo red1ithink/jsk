8affeed58f8648c122bc17646c881c0e66d7f09b
red1ithink <wntjd833@khu.ac.kr>
2025-03-12 23:39:47 +0900

    commit

--- archive.js
+++ archive.js
@@ -66,3 +66,13 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
   
+
+  function toggleMenu() {
+    const menu = document.getElementById('menu');
+    if (menu.style.maxHeight === '0px' || !menu.style.maxHeight) {
+      menu.style.maxHeight = '300px'; // 메뉴 표시 (내용 크기에 맞게 조정)
+    } else {
+      menu.style.maxHeight = '0px'; // 메뉴 숨김
+    }
+  }
+  
